aa383aa63cef3c7cf08c25eb9654fe53ac01b06a
Maximiliano Barata <maxibarata@gmail.com>
2025-02-17 22:26:52 -0300

    Django

--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def saludar(request):
+    return HttpResponse("Hola desde Djangoooo")
+
+def saludar_con_parametros(request, nombre:str, apellido:str):
+    nombre = nombre.capitalize()
+    apellido = apellido.capitalize()
+    return HttpResponse(f"Mi nombre es {nombre} y mi apellido {apellido}")
+
+def index (request):
+    return render(request, "core/index.html")
